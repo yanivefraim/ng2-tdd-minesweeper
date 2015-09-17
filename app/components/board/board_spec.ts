b95627d0182ecb07b1b437cc54717e1567294140
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-09-17 21:48:10 +0300

    added another test

--- app/components/board/board_spec.ts
+++ app/components/board/board_spec.ts
@@ -30,20 +30,25 @@ export function main() {
           let board = new Board(mockservice);
           expect(board.cells).toEqual(testArray);
         });
-        it('should gemerate component cells',
+        it('should generate component cells',
           inject([TestComponentBuilder, AsyncTestCompleter], (tcb: TestComponentBuilder, async) => {
             tcb.overrideTemplate(TestComponent, '<div><board></board></div>')
               .createAsync(TestComponent)
               .then((rootTC) => {
                 rootTC.detectChanges();
 
-                var boardInstance = rootTC.componentViewChildren[0].componentInstance;
-                var boardDOMEl = rootTC.componentViewChildren[0].nativeElement;
-                var mineListLen = function () {
+                let boardInstance = rootTC.componentViewChildren[0].componentInstance;
+                let boardDOMEl = rootTC.componentViewChildren[0].nativeElement;
+                let mineRowsLen = () => {
                   return boardInstance.cells.length;
                 }
+                let mineCellsLen = () => {
+                    return boardInstance.cells[0].length * mineRowsLen();
+                }
 
-                expect(DOM.querySelectorAll(boardDOMEl, 'tr').length).toEqual(mineListLen());
+                expect(DOM.querySelectorAll(boardDOMEl, 'tr').length).toEqual(mineRowsLen());
+
+                expect(DOM.querySelectorAll(boardDOMEl, 'td').length).toEqual(mineCellsLen());
 
                 async.done();
               });
