c392470c0b7b2a219f869b711dfa6e7a60a81974
oricalvo <ori.calvo@gmail.com>
2015-09-17 21:56:18 +0300

    Adding some CSS

--- app/services/MineService.ts
+++ app/services/MineService.ts
@@ -1,5 +1,14 @@
 export class MineService {
-  mines = [[0, 0, 1, 1, 0, 1, 0], [1, 0, 1, 1, 0, 1, 0], [0, 0, 1, 0, 0, 1, 0], [0, 0, 1, 1, 0, 0, 1]];//TODO: Generate random arrays
+    mines = [
+        [0, 0, 1, 1, 0, 1, 0],
+        [1, 0, 1, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 0, 1],
+        [0, 0, 1, 1, 0, 0, 1],
+        [0, 0, 1, 1, 0, 0, 1],
+    ];
+    
+    //TODO: Generate random arrays
 
   generate() {
     return this.mines;
