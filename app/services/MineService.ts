68b4c65a3a0d68864ca6e7b0f7e5c69741685dad
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-09-17 21:41:22 +0300

    comments

--- app/services/MineService.ts
+++ app/services/MineService.ts
@@ -1,5 +1,5 @@
 export class MineService {
-  mines = [[0, 0, 1, 1, 0, 1, 0], [1, 0, 1, 1, 0, 1, 0], [0, 0, 1, 0, 0, 1, 0], [0, 0, 1, 1, 0, 0, 1]];
+  mines = [[0, 0, 1, 1, 0, 1, 0], [1, 0, 1, 1, 0, 1, 0], [0, 0, 1, 0, 0, 1, 0], [0, 0, 1, 1, 0, 0, 1]];//TODO: Generate random arrays
 
   generate() {
     return this.mines;
